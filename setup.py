3c7592b0898dbefefa97889562ad55a8b33e55f9
stevenyuser <77898813+stevenyuser@users.noreply.github.com>
2021-05-17 17:39:44 -0400

    v1.1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0'
+VERSION = '1.1'
 DESCRIPTION = 'Converts a country code or country name into a flag emoji'
 LONG_DESCRIPTION = 'A Python package that converts a country code or country name into a flag emoji.'
 
